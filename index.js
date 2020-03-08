533feca73776067a20aa527d32458f108abcce52
Aaron Ault <aultac@gmail.com>
2020-03-07 21:55:49 -0500

    Fixed name on validate require in index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = {
   // Sign a client certificate:
   sign: require('./sign'),
   // Validate a client certificate (also check trusted):
-  validate: require('./verify'),
+  validate: require('./validate'),
   // Handy JWK utilities like jwkForSignature and decodeWithoutVerify
   jwksutils: require('./jwks-utils'),
   // Wrapper for node-jose to create an RSA public/private keypair for you (2048 bit key length)
